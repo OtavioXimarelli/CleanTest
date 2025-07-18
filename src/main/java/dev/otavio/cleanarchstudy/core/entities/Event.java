d7ef14a025d7eb2a3585357e7068aad6e7271589
otavio <ximarellio@gmail.com>
2025-07-18 15:46:20 -0300

    add Event entity and findEvent implementation

---
+++ src/main/java/dev/otavio/cleanarchstudy/core/entities/Event.java
@@ -0,0 +1,21 @@
+package dev.otavio.cleanarchstudy.core.entities;
+
+import dev.otavio.cleanarchstudy.core.enums.EventTypes;
+
+import java.time.LocalDateTime;
+
+public record Event(
+        Long id, String Name,
+        String Description,
+        String Location,
+        LocalDateTime StartDate,
+        LocalDateTime EndDate,
+        String Owner,
+        String Contact,
+        String Type,
+        EventTypes Category,
+        Integer Capacity,
+        String Identification
+
+) {
+}
---
+++ src/main/java/dev/otavio/cleanarchstudy/core/usecases/findEventImpl.java
@@ -0,0 +1,10 @@
+package dev.otavio.cleanarchstudy.core.usecases;
+
+import dev.otavio.cleanarchstudy.core.entities.Event;
+
+public class findEventImpl implements findEventCase{
+    @Override
+    public Event execute(Event event) {
+        return null;
+    }
+}
