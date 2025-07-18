cd028e675658a17fd386eae9efffc5170fab612f
otavio <ximarellio@gmail.com>
2025-07-18 15:46:12 -0300

    add createEventCase interface for event creation use case

---
+++ src/main/java/dev/otavio/cleanarchstudy/core/usecases/findEventCase.java
@@ -0,0 +1,8 @@
+package dev.otavio.cleanarchstudy.core.usecases;
+
+import dev.otavio.cleanarchstudy.core.entities.Event;
+
+public interface findEventCase {
+
+    public Event execute(Event event);
+}
