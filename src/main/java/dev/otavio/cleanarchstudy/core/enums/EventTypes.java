59b06f3dba197594a58f59e802516778002c9516
otavio <ximarellio@gmail.com>
2025-07-18 15:46:08 -0300

    add createEventCase interface for event creation use case

---
+++ src/main/java/dev/otavio/cleanarchstudy/core/enums/EventTypes.java
@@ -0,0 +1,11 @@
+package dev.otavio.cleanarchstudy.core.enums;
+
+public enum EventTypes {
+
+        CONCERT,
+        SEMINAR,
+        MEETUP,
+        WEBINAR,
+        FESTIVAL,
+        EXHIBITION
+}
