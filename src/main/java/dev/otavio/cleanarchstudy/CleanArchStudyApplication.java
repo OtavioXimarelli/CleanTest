571e8696f7f6c32d12f7e26f47ad0e456da617a6
otavio <ximarellio@gmail.com>
2025-07-18 15:43:23 -0300

    initialize CleanArchStudy application with main class and test setup

---
+++ src/main/java/dev/otavio/cleanarchstudy/CleanArchStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.otavio.cleanarchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/otavio/cleanarchstudy/CleanArchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otavio.cleanarchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
