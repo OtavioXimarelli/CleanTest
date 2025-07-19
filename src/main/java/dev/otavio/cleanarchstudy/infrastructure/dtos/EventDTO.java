14f00894a24a86549aebd56e77807c3d508889f7
otavio <ximarellio@gmail.com>
2025-07-19 13:42:09 -0300

    feat(dto): add EventDTO for event data transfer

---
+++ src/main/java/dev/otavio/cleanarchstudy/infrastructure/dtos/EventDTO.java
@@ -0,0 +1,21 @@
+package dev.otavio.cleanarchstudy.infrastructure.dtos;
+
+import dev.otavio.cleanarchstudy.core.enums.EventTypes;
+
+import java.time.LocalDateTime;
+
+public record EventDTO(
+        Long id,
+        String Name,
+        String Description,
+        String Location,
+        LocalDateTime StartDate,
+        LocalDateTime EndDate,
+        String Owner,
+        String Contact,
+        String Type,
+        EventTypes Category,
+        Integer Capacity,
+        String Identification
+) {
+}
