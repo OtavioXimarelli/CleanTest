fa921c551172f6f9088ce5b620064a6c51ea432b
otavio <ximarellio@gmail.com>
2025-07-19 17:29:40 -0300

    feat(controller): add EventController for event creation

---
+++ src/main/java/dev/otavio/cleanarchstudy/infrastructure/presentation/EventController.java
@@ -0,0 +1,33 @@
+package dev.otavio.cleanarchstudy.infrastructure.presentation;
+
+
+import dev.otavio.cleanarchstudy.core.entities.Event;
+import dev.otavio.cleanarchstudy.core.usecases.CreateEventCase;
+import dev.otavio.cleanarchstudy.infrastructure.dtos.EventDTO;
+import dev.otavio.cleanarchstudy.infrastructure.mapper.EventMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/events")
+public class EventController {
+
+    private final CreateEventCase createEventCase;
+    private final EventMapper eventMapper;
+
+    public EventController(CreateEventCase createEventCase, EventMapper eventMapper) {
+        this.createEventCase = createEventCase;
+        this.eventMapper = eventMapper;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<EventDTO> createEvent(@RequestBody EventDTO eventDTO) {
+        Event newEvent = createEventCase.execute(eventMapper.mapToEntity(eventDTO));
+        var eventDtoFinal = eventMapper.mapToDto(newEvent);
+        return ResponseEntity.ok(eventDtoFinal);
+
+    }
+}
