cefd0438867a2a60b817573eb4f9549f753a0c38
otavio <ximarellio@gmail.com>
2025-07-21 23:44:29 -0300

    feat(gateway): implement EventRepositoryGateway

---
+++ src/main/java/dev/otavio/cleanarchstudy/infrastructure/gateway/EventRepositoryGateway.java
@@ -0,0 +1,6 @@
+package dev.otavio.cleanarchstudy.infrastructure.gateway;
+
+import dev.otavio.cleanarchstudy.core.gateway.EventGateway;
+
+public class EventRepositoryGateway implements EventGateway {
+}
