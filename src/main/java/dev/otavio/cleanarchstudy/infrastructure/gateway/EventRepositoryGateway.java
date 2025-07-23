633217ec956544c98323cbe03c9422563ddcf139
otavio <ximarellio@gmail.com>
2025-07-22 23:36:33 -0300

    feat(gateway): implement createEvent method in EventRepositoryGateway

--- src/main/java/dev/otavio/cleanarchstudy/infrastructure/gateway/EventRepositoryGateway.java
+++ src/main/java/dev/otavio/cleanarchstudy/infrastructure/gateway/EventRepositoryGateway.java
@@ -1,6 +1,28 @@
 package dev.otavio.cleanarchstudy.infrastructure.gateway;
 
+import dev.otavio.cleanarchstudy.core.entities.Event;
 import dev.otavio.cleanarchstudy.core.gateway.EventGateway;
+import dev.otavio.cleanarchstudy.infrastructure.mapper.EventEntityMapper;
+import dev.otavio.cleanarchstudy.infrastructure.persistence.EventEntity;
+import dev.otavio.cleanarchstudy.infrastructure.persistence.EventRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EventRepositoryGateway implements EventGateway {
+
+    private final EventRepository eventRepository;
+    private final EventEntityMapper eventEntityMapper;
+
+    public EventRepositoryGateway(EventRepository eventRepository, EventEntityMapper eventEntityMapper) {
+        this.eventRepository = eventRepository;
+        this.eventEntityMapper = eventEntityMapper;
+    }
+
+
+    @Override
+    public Event createEvent(Event event) {
+        EventEntity entity = eventEntityMapper.toEventEntity(event);
+        EventEntity savedEvent = eventRepository.save(entity);
+        return eventEntityMapper.toEventDomain(savedEvent);
+    }
 }
