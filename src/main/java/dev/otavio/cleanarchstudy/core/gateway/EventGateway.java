54184918b70a8ced03a04b604d9a7231d996f904
otavio <ximarellio@gmail.com>
2025-07-22 23:36:25 -0300

    feat(gateway): add createEvent method to EventGateway

--- src/main/java/dev/otavio/cleanarchstudy/core/gateway/EventGateway.java
+++ src/main/java/dev/otavio/cleanarchstudy/core/gateway/EventGateway.java
@@ -1,4 +1,8 @@
 package dev.otavio.cleanarchstudy.core.gateway;
 
+import dev.otavio.cleanarchstudy.core.entities.Event;
+
 public interface EventGateway {
+
+    public Event createEvent (Event event);
 }
