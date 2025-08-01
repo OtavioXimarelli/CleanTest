bad4197d6ecf947f16bd8b7473f0ac17f548c966
otavio <ximarellio@gmail.com>
2025-08-01 20:00:38 -0300

    feat(config): update createEventCase to use EventGateway

--- src/main/java/dev/otavio/cleanarchstudy/infrastructure/config/BeansConfig.java
+++ src/main/java/dev/otavio/cleanarchstudy/infrastructure/config/BeansConfig.java
@@ -1,17 +1,19 @@
 package dev.otavio.cleanarchstudy.infrastructure.config;
 
 
+import dev.otavio.cleanarchstudy.core.gateway.EventGateway;
 import dev.otavio.cleanarchstudy.core.usecases.CreateEventCase;
 import dev.otavio.cleanarchstudy.core.usecases.CreateEventImpl;
 import dev.otavio.cleanarchstudy.infrastructure.mapper.EventEntityMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class BeansConfig {
     @Bean
-    public CreateEventCase createEventCase() {
-        return new CreateEventImpl();
+    public CreateEventCase createEventCase(EventGateway eventGateway) {
+        return new CreateEventImpl(eventGateway);
     }
 
     @Bean
