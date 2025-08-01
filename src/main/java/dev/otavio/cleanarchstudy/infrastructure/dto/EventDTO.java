bb18386c2b49be3f9093bb1b9284d8c0872db9bc
otavio <ximarellio@gmail.com>
2025-08-01 20:01:11 -0300

    feat(dto): add JSON properties to EventDTO fields

--- src/main/java/dev/otavio/cleanarchstudy/infrastructure/dto/EventDTO.java
+++ src/main/java/dev/otavio/cleanarchstudy/infrastructure/dto/EventDTO.java
@@ -1,21 +1,34 @@
 package dev.otavio.cleanarchstudy.infrastructure.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.otavio.cleanarchstudy.core.enums.EventTypes;
 
 import java.time.LocalDateTime;
 
 public record EventDTO(
+        @JsonProperty("id")
         Long id,
+        @JsonProperty("name")
         String Name,
+        @JsonProperty("description")
         String Description,
+        @JsonProperty("location")
         String Location,
+        @JsonProperty("startDate")
         LocalDateTime StartDate,
+        @JsonProperty("endDate")
         LocalDateTime EndDate,
+        @JsonProperty("owner")
         String Owner,
+        @JsonProperty("contact")
         String Contact,
+        @JsonProperty("type")
         String Type,
+        @JsonProperty("category")
         EventTypes Category,
+        @JsonProperty("capacity")
         Integer Capacity,
+        @JsonProperty("identification")
         String Identification
 ) {
 }
