0b9bb246830b48f3b1a6a483f98fa42e1d836eb0
otavio <ximarellio@gmail.com>
2025-08-01 20:00:46 -0300

    feat(usecases): implement event creation in CreateEventImpl

--- src/main/java/dev/otavio/cleanarchstudy/core/usecases/CreateEventImpl.java
+++ src/main/java/dev/otavio/cleanarchstudy/core/usecases/CreateEventImpl.java
@@ -1,10 +1,19 @@
 package dev.otavio.cleanarchstudy.core.usecases;
 
 import dev.otavio.cleanarchstudy.core.entities.Event;
+import dev.otavio.cleanarchstudy.core.gateway.EventGateway;
 
 public class CreateEventImpl implements CreateEventCase {
+
+    private final EventGateway eventGateway;
+
+    public CreateEventImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+
     @Override
     public Event execute(Event event) {
-        return null;
+         return eventGateway.createEvent(event);
     }
 }
