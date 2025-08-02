14621ca38b59dde36ee8df302038bd6b00d7dbdf
otavio <ximarellio@gmail.com>
2025-08-02 11:32:31 -0300

    feat(gateway): add method to list events

--- src/main/java/dev/otavio/cleanarchstudy/core/gateway/EventGateway.java
+++ src/main/java/dev/otavio/cleanarchstudy/core/gateway/EventGateway.java
@@ -2,7 +2,11 @@ package dev.otavio.cleanarchstudy.core.gateway;
 
 import dev.otavio.cleanarchstudy.core.entities.Event;
 
+import java.util.List;
+
 public interface EventGateway {
 
     public Event createEvent (Event event);
+
+    public List<Event> listEvent (Event event);
 }
