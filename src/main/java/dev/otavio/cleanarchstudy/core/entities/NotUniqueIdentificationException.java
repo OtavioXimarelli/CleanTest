792ebd1ce36b30b018e893bf6780226911af27e4
otavio <ximarellio@gmail.com>
2025-08-06 16:08:38 -0300

    feat(gateway): add method to check event identifier existence

--- src/main/java/dev/otavio/cleanarchstudy/core/entities/NotUniqueIdentificationException.java
+++
@@ -1,7 +0,0 @@
-package dev.otavio.cleanarchstudy.core.entities;
-
-public class NotUniqueIdentificationException extends RuntimeException {
-    public NotUniqueIdentificationException(String message) {
-        super(message);
-    }
-}
