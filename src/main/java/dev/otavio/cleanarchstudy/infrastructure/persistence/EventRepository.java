c0c7c64de04d1288e2c51bdf1c121481280f8845
otavio <ximarellio@gmail.com>
2025-08-07 22:29:43 -0300

    feat(repository): add method to find event by identification

--- src/main/java/dev/otavio/cleanarchstudy/infrastructure/persistence/EventRepository.java
+++ src/main/java/dev/otavio/cleanarchstudy/infrastructure/persistence/EventRepository.java
@@ -1,6 +1,12 @@
 package dev.otavio.cleanarchstudy.infrastructure.persistence;
 
+import dev.otavio.cleanarchstudy.core.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+    Optional<Event> findByIdentification(String identification);
+
 }
